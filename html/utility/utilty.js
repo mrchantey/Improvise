e461644c3c745bff6575136c775fb563bf9c92c7
Peter Hayman <peteyhayman@gmail.com>
2018-05-27 00:17:41 +1000

    Restart
    Start again from scratch:
    Client:
        Javascript
        Vue CLI
        Socket.io
    Server:
        Python
        Flask
        Socket IO
        Naoqi

--- html/utility/utilty.js
+++
@@ -1,5 +0,0 @@
-function SplitAndValidateParagraph(paragraph) {
-    const split = paragraph.split('.')
-    const validSplit = split.filter(s => s !== "");
-    return validSplit.map(s => s + '.')
-}
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const server = app.listen(port)
-
-app.use(express.static('html'))
-
-console.log('server running on port ' + port)
